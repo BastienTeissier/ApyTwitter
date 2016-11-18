c5bb6faffcf57d15d874881a35d6fc802a76ef68
Flo <flemarignier@hotmail.fr>
2016-11-18 09:33:39 +0100

    [Models] Added a model for filters

--- apyTwitter/tweetsChecker/models.py
+++ apyTwitter/tweetsChecker/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from .controllers.flag import Flag
-
-# Create your models here.
+from .controllers.filt import Filt
 
 
 class FlagModel(models.Model):
@@ -27,4 +26,32 @@ class FlagModel(models.Model):
         FlagModel.objects.filter(name=name).delete()
 
     def __str__(self):
-        return str(self.name + ": " + self.key_words)
+        return '{} : {}'.format(self.name, self.key_words)
+
+
+class FiltModel(models.Model):
+    name = models.CharField(max_length=100)
+    key_words = models.CharField(max_length=500)
+    count = models.IntegerField()
+
+    def to_model(self, filt):
+        self.name = filt.name
+        self.count = filt.count
+        key_words = ""
+        for word in filt.key_words:
+            if key_words == "":
+                key_words = word
+            else:
+                key_words = key_words + "+" + word
+        self.key_words = key_words
+
+    def to_filt(self):
+        key_words = self.key_words
+        return Filt(self.name, key_words.split("+"), self.count)
+
+    @staticmethod
+    def delete_filt(name):
+        FiltModel.objects.filter(name=name).delete()
+
+    def __str__(self):
+        return "{} ({}) : {}".format(self.name, self.count, self.key_words)
