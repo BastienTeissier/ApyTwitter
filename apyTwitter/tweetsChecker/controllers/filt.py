83c37eeff32cb4d249c9d04efdd753ae0b089b38
Guillaume L <Guillaume L>
2016-11-18 10:21:47 +0100

    Ajout exceptions

--- apyTwitter/tweetsChecker/controllers/filt.py
+++ apyTwitter/tweetsChecker/controllers/filt.py
@@ -1,8 +1,30 @@
 class Filt:
-    def __init__( self, name, key_words, count = 100 ):
-        self._name = name
-        self._key_words = key_words #key_words est une liste de key words
-        self.count = count
+    def __init__(self, name, key_words, count = 100 ):
+
+        if not isinstance(name,str):
+            raise TypeError("Le nom du filtre doit être une chaine de caractères")
+        if not isinstance(key_words, list):
+            raise TypeError("key_words doit être une liste")
+        for words in key_words:
+            if not isinstance(words,str):
+                raise TypeError("key_words doit être une liste de chaines de caractères")
+        if not isinstance(count, int):
+            raise TypeError("Entrez un nombre entier de tweets")
+        for words in key_words:
+            if words == "":
+                raise ValueError("Entrez des mots clés corrects")
+        if name == "":
+            raise ValueError("Entrez un nom de filtre correct")
+        if count <= 0:
+            raise ValueError("Entrez un nombre de tweets positifs")
+        if count > 100:
+            raise ValueError("Entrez un nombre inférieur à 100")
+
+
+        self._name = name           #le nom de notre filtre
+        self._key_words = key_words #la liste des key_words
+        self.count = count          #le nombre de tweets affichés
+
 
     @property
     def name(self):
@@ -15,7 +37,6 @@ class Filt:
     def dico(self):
         str_key_words = ""
         print(self._key_words)
-        dico = {}
         for words in self._key_words:
             if str_key_words == "":
                 str_key_words = words
@@ -32,4 +53,4 @@ if __name__ == "__main__":
     print(filt1.name)
     print(filt1.count)
     print(filt1.key_words)
-    print(filt1.dico)
+    print(filt1.dico())
--- apyTwitter/tweetsChecker/controllers/flag.py
+++ apyTwitter/tweetsChecker/controllers/flag.py
@@ -1,7 +1,35 @@
 class Flag:
     def __init__(self, name, key_words):
-        self.name = name
+
+        if not isinstance(name,str):
+            raise TypeError("Le nom du flag doit être une chaine de caractères")
+        if not isinstance(key_words,list):
+            raise TypeError("key_words doit être une liste")
+        for word in key_words:
+            if not isinstance(word,str):
+                raise TypeError("Les mots clés doivent être des chaines de caractères")
+        if name == "":
+            raise ValueError("Entrez un nom de flag correct")
+        for word in key_words:
+            if word == "":
+                raise ValueError("Entrez des mots clés corrects")
+
+        self._name = name               #le nom du flag
+
         keys = []
         for word in key_words:
             keys.append(word.lower())
-        self.key_words =keys
+        self._key_words = keys          #la liste des mots clés du flag
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def key_words(self):
+        return self._key_words
+
+if __name__ == "__main__":
+    flag1 = Flag("Politics", ["Obama"])
+    print(flag1.name)
+    print(flag1.key_words)
