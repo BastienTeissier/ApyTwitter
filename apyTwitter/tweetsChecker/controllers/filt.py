b650977b5c842d5b9b0c95fca6db473bcd6817d3
Flo <flemarignier@hotmail.fr>
2016-11-22 15:06:09 +0100

    Solved conflict on filt

--- apyTwitter/tweetsChecker/controllers/filt.py
+++ apyTwitter/tweetsChecker/controllers/filt.py
@@ -1,8 +1,43 @@
 class Filt:
-    def __init__( self, name, key_words, count = 100 ):
+    def __init__(self, name, key_words, count = 100 ):
+        '''
+
+        :param name: nom du filtre
+        :param key_words: liste des mots clés
+        :param count: nombre de tweets affichés
+
+        '''
+
+        if not isinstance(name,str):
+            raise TypeError("Le nom du filtre doit être une chaine de caractères")
+        if not isinstance(key_words, list):
+            raise TypeError("key_words doit être une liste")
+        for words in key_words:
+            if not isinstance(words,str):
+                raise TypeError("key_words doit être une liste de chaines de caractères")
+        if not isinstance(count, int):
+            raise TypeError("Entrez un nombre entier de tweets")
+        for words in key_words:
+            if words.strip() == "":
+                raise ValueError("Entrez des mots clés corrects")
+        if name.strip() == "":
+            raise ValueError("Entrez un nom de filtre correct")
+        if count <= 0:
+            raise ValueError("Entrez un nombre de tweets positifs")
+        if count > 100:
+            raise ValueError("Entrez un nombre inférieur à 100")
+
         self._name = name
-        self._key_words = key_words #key_words est une liste de key words
         self.count = count
+
+        # Mets les mots clès en minuscule
+
+        keys = []
+        for word in key_words:
+            keys.append(word.lower())
+        self._key_words = keys
+
+    # Créer des properties pour tous les attributs sauf count. La modification des filtres se fait par ajout de nouveaux filtres ou suppression d'existants
 
     @property
     def name(self):
@@ -12,19 +47,18 @@ class Filt:
     def key_words(self):
         return self._key_words
 
+    # Méthode transformant la liste de key_words initiale en dictionnaire assimilable par l'API de Twitter
     def dico(self):
         str_key_words = ""
-        print(self._key_words)
-        dico ={}
         for words in self._key_words:
             if str_key_words == "":
                 str_key_words = words
             else:
-                str_key_words += '+' + words #on transforme notre liste de key words en une chaine de caractères assimilable par l'API
+                str_key_words += '+' + words
         dico = {
             'q': str_key_words,
             'count' : self.count
-            }
+        }
         return dico
 
 if __name__ == "__main__":
@@ -32,4 +66,4 @@ if __name__ == "__main__":
     print(filt1.name)
     print(filt1.count)
     print(filt1.key_words)
-    print(filt1.dico)
+    print(filt1.dico())
